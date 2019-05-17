0153e3000fe18c41a82fbee4ea32163ad2f89b2c
jakehilborn <jakehilborn@gmail.com>
2019-05-17 16:08:24 -0700

    v1.3.0-dev

--- displayplacer.c
+++ displayplacer.c
@@ -215,7 +215,7 @@ void printHelp() {
 
 void printVersion() {
     printf(
-        "displayplacer v1.2.0\n"
+        "displayplacer v1.3.0-dev\n"
         "\n"
         "Developer: Jake Hilborn\n"
         "GitHub: https://github.com/jakehilborn/displayplacer\n"
