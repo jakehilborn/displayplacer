31aa1c2bffd88e877c2e95e0191d5ebbd091c74d
Jake Hilborn <jakehilborn@gmail.com>
2023-06-03 10:30:39 -0700

    remove unused functions

--- Headers/Bridging-Header.h
+++ Headers/Bridging-Header.h
@@ -1,3 +1,5 @@
+//Thanks to Alin Panaitiu for this header file to access the MonitorPanel framework: https://github.com/alin23/mac-utils
+
 #import <CoreGraphics/CoreGraphics.h>
 #import <CommonCrypto/CommonDigest.h>
 #import <Foundation/Foundation.h>
@@ -6,29 +8,6 @@
 #import <MonitorPanel/MPDisplayMgr.h>
 #import <MonitorPanel/MPDisplayMode.h>
 
-double CoreDisplay_Display_GetUserBrightness(CGDirectDisplayID display);
-double CoreDisplay_Display_GetLinearBrightness(CGDirectDisplayID display);
-double CoreDisplay_Display_GetDynamicLinearBrightness(CGDirectDisplayID display);
-
-void CoreDisplay_Display_SetUserBrightness(CGDirectDisplayID display, double brightness);
-void CoreDisplay_Display_SetLinearBrightness(CGDirectDisplayID display, double brightness);
-void CoreDisplay_Display_SetDynamicLinearBrightness(CGDirectDisplayID display, double brightness);
-
-void CoreDisplay_Display_SetAutoBrightnessIsEnabled(CGDirectDisplayID, bool);
-
 CFDictionaryRef CoreDisplay_DisplayCreateInfoDictionary(CGDirectDisplayID);
-
-int DisplayServicesGetLinearBrightness(CGDirectDisplayID display, float *brightness);
-int DisplayServicesSetLinearBrightness(CGDirectDisplayID display, float brightness);
-int DisplayServicesGetBrightness(CGDirectDisplayID display, float *brightness);
-int DisplayServicesSetBrightness(CGDirectDisplayID display, float brightness);
-int DisplayServicesSetBrightnessSmooth(CGDirectDisplayID display, float brightness);
-bool DisplayServicesCanChangeBrightness(CGDirectDisplayID display);
-bool DisplayServicesHasAmbientLightCompensation(CGDirectDisplayID display);
-bool DisplayServicesAmbientLightCompensationEnabled(CGDirectDisplayID display);
-bool DisplayServicesIsSmartDisplay(CGDirectDisplayID display);
-void DisplayServicesBrightnessChanged(CGDirectDisplayID display, double brightness);
-
 extern int SLSMainConnectionID(void);
 CGError SLSGetDisplayList(uint32_t maxDisplays, CGDirectDisplayID *activeDisplays, uint32_t *displayCount);
-CGError SLSGetZoomParameters(int cid, CGPoint *origin, double *zoomFactor, bool *smoothed);
