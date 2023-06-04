12fbca4fe504ab4388cbfd696820901d3439fbd3
Jake Hilborn <jakehilborn@gmail.com>
2023-06-03 17:42:44 -0700

    Give screens 10 seconds to rotate

--- src/MonitorPanel.m
+++ src/MonitorPanel.m
@@ -1,22 +1,31 @@
 #include "Headers/Bridging-Header.h"
 
 bool setRotation(CGDirectDisplayID screenId, char* screenUUID, int degree) {
+    bool isSuccess = true;
+
     MPDisplay* mpDisplay = [[MPDisplay alloc] initWithCGSDisplayID:screenId];
     [mpDisplay setOrientation: degree];
 
-    while(true) {
-        MPDisplay* mpDisplayRotated = [[MPDisplay alloc] initWithCGSDisplayID:screenId];
-        int curDegree = mpDisplayRotated.orientation;
-        [mpDisplayRotated release];
+    // block until non-blocking rotation change completes
+    int waitSeconds = 10;
+    unsigned long beginTime = time(NULL);
+    unsigned long secondsElapsed = 0;
+    while (CGDisplayRotation(screenId) != degree ) {
+        unsigned long curSecondsElapsed = time(NULL) - beginTime;
 
-        if (curDegree == degree) {
-            break;
-        } else {
-            printf("Waiting for rotation to apply on screen id: %s\n", screenUUID);
-            // wait until non-blocking rotation change is complete
+        if (curSecondsElapsed > secondsElapsed) {
+            printf("Waited %lu of %i seconds for screen %s to complete rotating\n", curSecondsElapsed, waitSeconds, screenUUID);
         }
+
+        if (curSecondsElapsed >= waitSeconds) {
+            fprintf(stderr, "Unable to confirm if screen %s successfully rotated to degree:%i\n", screenUUID, degree);
+            isSuccess = false;
+            break;
+        }
+
+        secondsElapsed = curSecondsElapsed;
     }
 
     [mpDisplay release];
-    return true;
+    return isSuccess;
 }
