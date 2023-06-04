8463f72dea2e404ac579cc156adb2b3bdc8934dd
Jake Hilborn <jakehilborn@gmail.com>
2023-06-04 09:54:08 -0700

    Apple Silicon tests

--- test/tests.py
+++ test/tests.py
@@ -8,7 +8,12 @@ def main():
     print('These tests only work on the original dev\'s multi monitor setup!')
     print('')
 
-    reset_conf()
+    intel()
+    # apple()
+
+
+def intel():
+    reset_conf_intel()
 
     print('Test profile that did not work in version 1.2')
     test('set_problematic_profile',
@@ -16,7 +21,7 @@ def main():
          'match_input',
          0,
          None)
-    reset_conf(expected_code=None)  # resetting after this profile used to not work. The screen rotations do work, but they time out. This is good enough, so we ignore the error code.
+    reset_conf_intel(expected_code=None)  # resetting after this profile used to not work. The screen rotations do work, but they time out. This is good enough, so we ignore the error code.
 
     print('Test disabling screen')
     test('enable_mirroring',
@@ -29,7 +34,7 @@ def main():
          'match_input',
          0,
          None)
-    reset_conf()
+    reset_conf_intel()
 
     print('Test missing screen')
     test('test_missing_screen_partial_error_conf_applied_for_non_missing_screens',
@@ -37,7 +42,7 @@ def main():
          '"id:A46D2F5E-487B-CC69-C588-ECFD519016E5 res:3840x2160 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:F466F621-B5FA-04A0-0800-CFA6C258DECD res:1440x900 color_depth:4 enabled:true scaling:on origin:(-1440,1469) degree:0" "id:34686E82-0CED-DF86-AFC7-AA1A8EB5CFC0 res:1440x2560 hz:59 color_depth:8 enabled:true scaling:off origin:(3840,-1109) degree:90" "id:EA487A4B-D9B9-DDDD-91F8-F43E599B7E84 res:1920x1200 color_depth:8 enabled:true scaling:off origin:(425,-1200) degree:0"',
          1,
          'Unable to find screen B46D2F5E-487B-CC69-C588-ECFD519016E5 - skipping changes for that screen')
-    reset_conf()
+    reset_conf_intel()
 
     print('Test missing screen with quiet mode')
     test('test_missing_screen_quiet_mode_suppresses_error',
@@ -45,7 +50,7 @@ def main():
          '"id:A46D2F5E-487B-CC69-C588-ECFD519016E5 res:3840x2160 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:F466F621-B5FA-04A0-0800-CFA6C258DECD res:1440x900 color_depth:4 enabled:true scaling:on origin:(-1440,1469) degree:0" "id:34686E82-0CED-DF86-AFC7-AA1A8EB5CFC0 res:1080x1920 hz:60 color_depth:8 enabled:true scaling:off origin:(3840,-1109) degree:90" "id:EA487A4B-D9B9-DDDD-91F8-F43E599B7E84 res:1600x1000 color_depth:8 enabled:true scaling:off origin:(425,-1000) degree:0"',
          0,
          None)
-    reset_conf()
+    reset_conf_intel()
 
     print('Test id not required if only one screen active')
     test('test_set_one_display_active_others_are_disabled',
@@ -58,7 +63,7 @@ def main():
          '"id:A46D2F5E-487B-CC69-C588-ECFD519016E5 res:1920x1080 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:F466F621-B5FA-04A0-0800-CFA6C258DECD enabled:false" "id:34686E82-0CED-DF86-AFC7-AA1A8EB5CFC0 enabled:false" "id:EA487A4B-D9B9-DDDD-91F8-F43E599B7E84 enabled:false"',
          0,
          None)
-    reset_conf()
+    reset_conf_intel()
 
     # TODO this is a known bug, but low priority. The code currently will disable mirroring for all screens before applying config. When the user does not pass in the fully described profile, we disable the mirroring but do not have the context to put the mirroring back. This could be fixed by internally calling `printCurrentProfile()` and using that to fill in the missing context for the current profile.
     # print('Test id not required if only one mirroring set active')
@@ -72,7 +77,7 @@ def main():
     #      '"id:A46D2F5E-487B-CC69-C588-ECFD519016E5+EA487A4B-D9B9-DDDD-91F8-F43E599B7E84 res:1920x1080 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:F466F621-B5FA-04A0-0800-CFA6C258DECD enabled:false" "id:34686E82-0CED-DF86-AFC7-AA1A8EB5CFC0 enabled:false"',
     #      0,
     #      None)
-    # reset_conf()
+    # reset_conf_intel()
 
     print('Test disabling and enabling a mirrored screen')
     test('test_set_mirror',
@@ -90,7 +95,37 @@ def main():
          expected_conf='match_input',
          expected_code=None,  # The screen rotations do work, but they time out. This is good enough so we ignore the error code.
          expected_error=None)
-    reset_conf(expected_code=None)  # The screen rotations do work, but they time out. This is good enough so we ignore the error code.
+    reset_conf_intel(expected_code=None)  # The screen rotations do work, but they time out. This is good enough so we ignore the error code.
+
+
+def apple():
+    reset_conf_apple()
+
+    print('Test profile that did not work in version 1.2')
+    test('set_problematic_profile',
+         '"id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1080 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:7746278E-D1CF-4842-8286-26BDEBFD9F1A+EF59C37B-E9D6-409F-B5B2-226384796D30 res:1080x1920 hz:60 color_depth:8 enabled:true scaling:off origin:(1728,-226) degree:90" "id:F2DDEF8B-97AE-4083-8C90-3B0B71B6858E res:1920x1200 hz:60 color_depth:8 enabled:true scaling:off origin:(-1920,-962) degree:0"',
+         'match_input',
+         0,
+         None)
+    reset_conf_apple(expected_code=None)  # resetting after this profile used to not work. The screen rotations do work, but they time out. This is good enough, so we ignore the error code.
+
+    print('Test disabling and enabling a mirrored screen')
+    test('test_set_mirror',
+         '"id:7746278E-D1CF-4842-8286-26BDEBFD9F1A res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:90" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230+EF59C37B-E9D6-409F-B5B2-226384796D30+F2DDEF8B-97AE-4083-8C90-3B0B71B6858E res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(-1728,1193) degree:0"',
+         'match_input',
+         0,
+         None)
+    test('test_disable_mirror_screen',
+         '"id:F2DDEF8B-97AE-4083-8C90-3B0B71B6858E res:1920x1200 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230+EF59C37B-E9D6-409F-B5B2-226384796D30 res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(-1728,1193) degree:0" "id:7746278E-D1CF-4842-8286-26BDEBFD9F1A res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(1920,-339) degree:90"',
+         'match_input',
+         0,
+         None)
+    test(step='test_enable_mirror_screen_and_rotate_180',
+         conf='"id:7746278E-D1CF-4842-8286-26BDEBFD9F1A res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:90" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230+EF59C37B-E9D6-409F-B5B2-226384796D30+F2DDEF8B-97AE-4083-8C90-3B0B71B6858E res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(-1728,1193) degree:180"',
+         expected_conf='match_input',
+         expected_code=None,  # The screen rotations do work, but they time out. This is good enough so we ignore the error code.
+         expected_error=None)
+    reset_conf_apple(expected_code=None)  # The screen rotations do work, but they time out. This is good enough so we ignore the error code.
 
 
 def test(step, conf, expected_conf, expected_code, expected_error):
@@ -119,9 +154,17 @@ def test(step, conf, expected_conf, expected_code, expected_error):
         raise e
 
 
-def reset_conf(expected_code=0):
+def reset_conf_intel(expected_code=0):
     test('reset_conf',
          '"id:A46D2F5E-487B-CC69-C588-ECFD519016E5 res:3840x2160 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:F466F621-B5FA-04A0-0800-CFA6C258DECD res:1440x900 color_depth:4 enabled:true scaling:on origin:(-1440,1469) degree:0" "id:34686E82-0CED-DF86-AFC7-AA1A8EB5CFC0 res:1440x2560 hz:59 color_depth:8 enabled:true scaling:off origin:(3840,-238) degree:90" "id:EA487A4B-D9B9-DDDD-91F8-F43E599B7E84 res:1920x1200 color_depth:8 enabled:true scaling:off origin:(960,-1200) degree:0"',
+         'match_input',
+         expected_code,
+         None)
+
+
+def reset_conf_apple(expected_code=0):
+    test('reset_conf',
+         '"id:EF59C37B-E9D6-409F-B5B2-226384796D30 res:3840x2160 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1496x967 hz:120 color_depth:8 enabled:true scaling:on origin:(-1496,1193) degree:0" "id:7746278E-D1CF-4842-8286-26BDEBFD9F1A res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(3840,-226) degree:90" "id:F2DDEF8B-97AE-4083-8C90-3B0B71B6858E res:1920x1200 hz:60 color_depth:8 enabled:true scaling:off origin:(960,-1200) degree:0"',
          'match_input',
          expected_code,
          None)
